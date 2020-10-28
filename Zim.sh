6a6c4c64222ce2d29929772fb2e1533a022606fe
Zim <73538267+Mohamm4@users.noreply.github.com>
2020-10-28 10:51:35 +0600

    Update Zim.sh

--- Zim.sh
+++ Zim.sh
@@ -2,13 +2,13 @@
 #This tool is created by Mohammad Zim
 #Target
 echo 'enter the ID name'
-read Target.
+read Target
 #Output
 echo 'enter ID link'
-read name.
+read name
 echo 'cracking ||||||||||||||||||||||||||||||'
 echo 'press enter'
-read name.
+read name
 clear
 echo 'your system has problems'
 
