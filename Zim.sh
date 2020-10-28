ededc61bd9cf42d1f0f34c060a928ee195ee44d9
Zim <73538267+Mohamm4@users.noreply.github.com>
2020-10-28 10:47:12 +0600

    Zim.sh
    useful tool

---
+++ Zim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#This tool is created by Mohammad Zim
+#Target
+echo 'enter the ID name'
+read Target.
+#Output
+echo 'enter ID link'
+read name.
+echo 'cracking ||||||||||||||||||||||||||||||'
+echo 'press enter'
+read name.
+clear
+echo 'your system has problems'
+
+clear
